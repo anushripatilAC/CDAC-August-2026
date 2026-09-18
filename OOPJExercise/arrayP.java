c25908411b6f45812693d3a3adcfc7732189b3a5
anushripatilAC <anushri.patil.cmaug26@gmail.com>
2026-09-18 22:34:02 +0530

    Add files via upload

---
+++ OOPJExercise/arrayP.java
@@ -0,0 +1,21 @@
+
+public class arrayP {
+
+	public static void main(String[] args) {
+		int [] marks = new int[5];
+		marks[0] = 10;
+		marks[2] = 40;      //put value into array
+		marks[3] = 60;
+		marks[4] = 70;
+
+		//access value from array
+		System.out.println(marks[0]);
+		System.out.println(marks[1]);
+		System.out.println(marks[2]);
+		System.out.println(marks[3]);
+		System.out.println(marks[4]);
+
+		
+	}
+
+}
