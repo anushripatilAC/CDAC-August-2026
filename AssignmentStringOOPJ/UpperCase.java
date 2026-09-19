fddce5eafcddc51050587d76a7a4a49fb6990fc9
anushripatilAC <anushri.patil.cmaug26@gmail.com>
2026-09-19 15:23:24 +0530

    Add files via upload

---
+++ AssignmentStringOOPJ/UpperCase.java
@@ -0,0 +1,13 @@
+
+public class UpperCase{
+	    public static void main(String[] args) {
+	        String str = "The Quick Brown Fox!";
+	        String upperStr = str.toUpperCase();
+
+	        System.out.println("Original String: " + str);
+	        System.out.println("String in uppercase: " + upperStr);
+	    }
+	}
+	
+
+
