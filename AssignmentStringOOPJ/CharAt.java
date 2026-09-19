9a76769fb7751950ed0b8ed2e58906e9e1b1f5f2
anushripatilAC <anushri.patil.cmaug26@gmail.com>
2026-09-19 15:21:02 +0530

    Add files via upload

---
+++ AssignmentStringOOPJ/CharAt.java
@@ -0,0 +1,18 @@
+
+public class CharAt {
+	    public static void main(String[] args) {
+	        String str = "Java Exercises!";
+	        
+	        // Retrieve characters at position 0 and 10
+	        char index0 = str.charAt(0);
+	        char index10 = str.charAt(10);
+	        
+	        // Display output
+	        System.out.println("Original String = " + str);
+	        System.out.println("The character at position 0 is " + index0);
+	        System.out.println("The character at position 10 is " + index10);
+	    }
+	}
+
+
+
