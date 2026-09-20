ad8a0f57ca87a8053d9234849c295d8c32ddc69a
anushripatilAC <anushri.patil.cmaug26@gmail.com>
2026-09-21 00:03:56 +0530

    Add files via upload

---
+++ OOPJ Assignment/ArraylistAssignment/ReverseList.java
@@ -0,0 +1,13 @@
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+public class ReverseList {
+		public static void main(String [] args) {
+			List <Integer> list = Arrays.asList(40,98,78,33,21,66);
+			Collections.reverse(list);
+			System.out.println(list);
+		}
+}
+
+	
