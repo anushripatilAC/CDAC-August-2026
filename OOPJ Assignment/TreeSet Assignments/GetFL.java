6dade73e7357ac7f923cb72859671282ced445be
anushripatilAC <anushri.patil.cmaug26@gmail.com>
2026-09-24 10:09:50 +0530

    Add files via upload

---
+++ OOPJ Assignment/TreeSet Assignments/GetFL.java
@@ -0,0 +1,17 @@
+import java.util.TreeSet;
+public class GetFL {
+	public static void main (String[] args) {
+		TreeSet <Integer> Number= new TreeSet<>();
+		Number.add(21);
+		Number.add(34);
+		Number.add(98);
+		Number.add(67);
+		
+		System.out.println(Number.first());
+		System.out.println(Number.last());
+		
+		
+	}
+	
+	}
+
