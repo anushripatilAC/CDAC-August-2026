2dbb7ac65baa29d7b511ca9834c05a211bbabb61
anushripatilAC <anushri.patil.cmaug26@gmail.com>
2026-09-24 10:07:06 +0530

    Add files via upload

---
+++ OOPJ Assignment/TreeSet Assignments/TreeSetColors.java
@@ -0,0 +1,17 @@
+package operation.com;
+import java.util.TreeSet;
+
+public class TreeSetColors {
+	public static void main(String[] args) {
+		TreeSet <String> set = new TreeSet<>();
+		set.add("pink");
+		set.add("blue");
+		set.add("red");
+		set.add("purple");
+		set.add("orange");
+		set.add("green");
+		
+		
+		System.out.println(set);
+	}
+}
